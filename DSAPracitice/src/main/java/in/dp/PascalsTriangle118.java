6fae16db19cad7003bbe48380edfdde3c2fdb34b
Omkar-the-codr <omkarchaturvedi2004@gmail.com>
2026-07-31 02:20:45 +0530

    adding the second question of the day

---
+++ DSAPracitice/src/main/java/in/dp/PascalsTriangle118.java
@@ -0,0 +1,26 @@
+package in.dp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalsTriangle118 {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int row=1;row<=numRows;row++){
+            ans.add(generateRow(row));
+        }
+        return ans;
+    }
+    private static List<Integer> generateRow(int row){
+        long ans = 1;
+        List<Integer> ansRow = new ArrayList<>();
+        ansRow.add(1);
+
+        for(int col = 1;col<row;col++){
+            ans = ans*(row-col);
+            ans = ans/col;
+            ansRow.add((int)ans);
+        }
+        return ansRow;
+    }
+}
