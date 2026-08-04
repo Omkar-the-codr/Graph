b3dea6d76b7df87e83b80528e2d976b9e8d24191
Omkar-the-codr <omkarchaturvedi2004@gmail.com>
2026-08-05 00:30:57 +0530

    adding the second question of the day

---
+++ DSAPracitice/src/main/java/in/tree/BinaryTreeInorderTraversal94.java
@@ -0,0 +1,31 @@
+package in.tree;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreeInorderTraversal94 {
+    List<Integer> list = new ArrayList<>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        inorder(root);
+        return list;
+    }
+    public void inorder(TreeNode root){
+        if(root==null) return;
+        inorder(root.left);
+        list.add(root.val);
+        inorder(root.right);
+    }
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }}
+
