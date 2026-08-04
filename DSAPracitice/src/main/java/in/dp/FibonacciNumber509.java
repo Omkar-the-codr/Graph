9da5f5601b470c05905bfd2606748e576e7105fc
Omkar-the-codr <omkarchaturvedi2004@gmail.com>
2026-08-05 00:24:43 +0530

    adding the first code of the day

---
+++ DSAPracitice/src/main/java/in/dp/FibonacciNumber509.java
@@ -0,0 +1,10 @@
+package in.dp;
+
+public class FibonacciNumber509 {
+    public int fib(int n) {
+        if(n==0) return 0;
+        if(n==1) return 1;
+        int fibo = fib(n-1) + fib(n-2);
+        return fibo;
+    }
+}
