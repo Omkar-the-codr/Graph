b1e5938a1e5f203af68eeca113c0347e9809be84
Omkar-the-codr <omkarchaturvedi2004@gmail.com>
2026-08-07 02:14:48 +0530

    adding the first code of the day

---
+++ DSAPracitice/src/main/java/in/tree/SymmetricTree101.java
@@ -0,0 +1,27 @@
+package in.tree;
+
+public class SymmetricTree101 {
+      class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
+
+        public boolean isSymmetric(TreeNode root) {
+            if(root==null) return true;
+            return recurse(root.left, root.right);
+        }
+        public boolean recurse(TreeNode left, TreeNode right){
+            if(left==null && right==null) return true;
+            else if(left==null || right==null) return false;
+            else if(left.val!=right.val) return false;
+            return recurse(left.left, right.right) && recurse(left.right, right.left);
+        }
+}
