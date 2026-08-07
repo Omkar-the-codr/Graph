9a26f2b7516175d068ca12fe309039fd5093d169
Omkar-the-codr <omkarchaturvedi2004@gmail.com>
2026-08-08 00:53:33 +0530

    adding the first code of the day

---
+++ DSAPracitice/src/main/java/in/tree/MaximumDepthOfBinaryTree104.java
@@ -0,0 +1,22 @@
+package in.tree;
+
+public class MaximumDepthOfBinaryTree104 {
+    class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+    public int maxDepth(TreeNode root) {
+        if(root==null) return 0;
+        int left= 1+maxDepth(root.left);
+        int right = 1+maxDepth(root.right);
+        return Math.max(left, right);
+    }
+}
